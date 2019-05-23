f780e2fa1f2a44945b0a0f260805626213149097
guangyongluo <llooww@mail.ustc.edu.cn>
2019-05-23 08:34:06 +0800

    hibernate(com.vilin.hibernate.util):create hibernate util class.

---
+++ src/main/java/com/vilin/hibernate/util/HibernateUtil.java
@@ -0,0 +1,38 @@
+package com.vilin.hibernate.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+	private static ServiceRegistry serviceRegistry;
+	private static SessionFactory sessionFactory;
+	private static ThreadLocal<Session> local = new ThreadLocal<Session>();
+	
+	static {
+		serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+		sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
+	}
+	
+	public static Session getSession() {
+		Session session = local.get();
+		if(session == null || !session.isOpen()) {
+			session = sessionFactory.openSession();
+			local.set(session);
+		}
+		return session;
+	}
+	
+	public static void close() {
+		Session session = local.get();
+		if(session != null) {
+			session.close();
+			local.remove();
+			sessionFactory.close();
+		}
+	
+	}
+}
